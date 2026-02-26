dae2768dde90e8323690c6578a7524bae8f3abe1
zack871024 <31832246+zack871024@users.noreply.github.com>
2026-02-25 17:41:29 -0800

    Add new product URLs to import script

--- 1_Import New Product.py
+++ 1_Import New Product.py
@@ -9,6 +9,9 @@ from io import StringIO
 
 urls = [
     'https://tcgcsv.com/tcgplayer/68/24575/ProductsAndPrices.csv',
+    'https://tcgcsv.com/tcgplayer/68/24537/ProductsAndPrices.csv',
+    'https://tcgcsv.com/tcgplayer/68/24545/ProductsAndPrices.csv',
+    'https://tcgcsv.com/tcgplayer/68/24579/ProductsAndPrices.csv',
     # Add more URLs as needed
 ]
 
